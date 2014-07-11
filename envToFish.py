718b8dcb87ae2b78e5ce0aded0504a81d599daf7
Sv3n <sgoossens@gmail.com>
2014-07-11 15:13:17 +0200

    Create envToFish.py
    Python script that emits the current environment in a format fish can parse.
    
    Future versions should probably make sure the created file does not exist yet, or alternatively emit the results to stdout such that it pipes directly into fish without leaving profile.fish files begin.

---
+++ envToFish.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+
+badKeys = ['HOME', 'PWD', 'USER', '_', 'OLDPWD']
+with open('profile.fish', 'w') as f:
+    for key, val in os.environ.items():
+        if key in badKeys:
+            continue
+        if key == 'PATH':
+            f.write("set -e PATH\n")
+            pathUnique = set(val.split(':'))
+            for elem in pathUnique:
+                f.write("set -gx PATH $PATH %s\n" % elem)
+        else:
+            f.write("set -gx %s '%s'\n" % (key, val))
